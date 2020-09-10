a1d541825ba410fa9c3adaa6a588e0ae64fc161f
Vasya Bogatchuk <v380962437840@gmail.com>
2020-09-11 00:37:22 +0300

    no message

---
+++ Hard-4_20/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hard_4_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст-> ");
+            string str = Convert.ToString(Console.ReadLine());
+            var result = String.Concat(str.Split(new Char[] { ' ', ',', '.', '!', '?' },
+            StringSplitOptions.RemoveEmptyEntries).Select(word => word.First()));
+            Console.WriteLine("Вервые буквы всех слов-> ", result);
+            Console.ReadKey();
+        }
+    }
+}
